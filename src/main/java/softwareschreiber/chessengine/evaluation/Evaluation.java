f2fda49b289884604db19badb344de95aa2241e0
Kevin Forst <kevinjuli@nforst.de>
2025-03-07 22:00:54 +0100

    Delete unused import

--- src/main/java/softwareschreiber/chessengine/evaluation/Evaluation.java
+++ src/main/java/softwareschreiber/chessengine/evaluation/Evaluation.java
@@ -1,7 +1,6 @@
 package softwareschreiber.chessengine.evaluation;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
