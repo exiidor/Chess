46e6a6ad6a2567bac67e045b9526e239f1421fad
NebelNidas <nebelnidas@gmail.com>
2025-05-08 16:12:01 +0200

    Update Gradle for Java 24 compatibility

---
+++ chess-gui/src/test/java/softwareschreiber/chess/gui/DummyTest.java
@@ -0,0 +1,11 @@
+package softwareschreiber.chess.gui;
+
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+	@Test
+	public void test() {
+		// This exists to prevent Gradle from logging a warning about no tests being executed at build time:
+		// https://docs.gradle.org/8.14/userguide/upgrading_version_8.html#test_task_fail_on_no_test_executed
+	}
+}
